1bfcc24d099a027c80206c3adc7fbc96ed713f45
Leo Scherhag <lee-jordan@hotmail.de>
2016-02-01 19:36:32 +0100

    CompareTo getestet

--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -8,6 +8,13 @@ namespace ConsoleApplication3 {
     class Program {
         static void Main(string[] args) {
 
+            //int Counter1, Counter2;
+            
+            //Counter1 = 5;
+            //Counter2 = 50;
+            
+            //Console.WriteLine (Counter2.CompareTo(Counter1));
+
             string strtext = "beispieltext";
             string firstname = "leo Git Test", lastname = "lutschi";
 
